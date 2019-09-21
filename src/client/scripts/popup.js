18679d47b1428815cd475c5f9befae3396687343
shegsteham <oluwasegunadepoju@gmail.com>
2019-09-21 21:53:00 +0200

    bg(background): implement extension background processes [finishes]

--- src/client/scripts/popup.js
+++ src/client/scripts/popup.js
@@ -2,19 +2,21 @@ const select = document.querySelector.bind(document);
 
 let changeColor = document.getElementById('changeColor');
 
- changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
+//  changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.executeScript(
+//           tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+//   };
 
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-  });
+//   chrome.storage.sync.get('color', function(data) {
+//     changeColor.style.backgroundColor = data.color;
+//     changeColor.setAttribute('value', data.color);
+//   });
+
+
   //check incognito
   // function saveTabData(tab) {
   //   if (tab.incognito) {
